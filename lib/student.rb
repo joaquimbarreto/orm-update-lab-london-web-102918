08c655f1cab9fccb5dd37793569130dacc60e5fa
joaquimbarreto <joaquim@barreto.co>
2018-11-12 02:18:25 +0000

    going to sleep

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,27 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  attr_accessor :id, :name, :grade
+
+  def initialize(name, grade)
+    @id = nil
+    @name = name
+    @grade = grade
+  end
+
+  def method_name
+    
+  end
+
+
+
+
+
+
+
+
+
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
